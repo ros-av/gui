de4d458cbcc9bae17aaf481b123b516d9e1b884e
Richie Bendall <richiebendall@gmail.com>
2019-03-07 19:04:14 +1300

    Update code

---
+++ app.js
@@ -0,0 +1,34 @@
+import $ from "jquery"
+
+console.log(localStorage)
+
+new Vue({
+    el: ".app",
+    data: {
+        activeTab: "dashboard"
+    },
+    methods: {
+        isActiveTab(tabId) {
+            return this.activeTab === tabId
+        },
+        setActiveTab(tabId) {
+            this.activeTab = tabId
+        }
+    }
+})
+
+const list = mdc.list.MDCList.attachTo(document.querySelector('.mdc-list'))
+list.wrapFocus = true
+mdc.autoInit()
+
+$.each($(".mdc-icon-button[data-mdc-auto-init='MDCRipple']"), (_, obj) => {
+    obj.MDCRipple.unbounded = true
+})
+
+$(".scan--directory-helper").change(() => {
+    $(".scan--directory").get(0).MDCTextField.value = $(".scan--directory-helper").get(0).files[0].path
+})
+
+$(".scan--directory-choose").click(() => {
+    $(".scan--directory-helper").click()
+})
