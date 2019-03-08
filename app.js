a728cf69c1d71b96db155a474c6157bb4b474f7b
Richie Bendall <richiebendall@gmail.com>
2019-03-08 23:38:22 +1300

    Update code

--- app.js
+++ app.js
@@ -30,16 +30,20 @@ const populateDirectory = (dir) => {
             fs.mkdir(dir, {
                 recursive: true
             }, (err) => {
-                if (err) throw err;
-            });
+                if (err) throw err
+            })
         }
-    });
+    })
 }
 
 // Define storage path
 const storage = path.join(require("temp-dir"), "rosav")
 
 populateDirectory(storage)
+populateDirectory(path.join(storage, "quarantine"))
+populateDirectory(path.join(storage, "reports"))
+populateDirectory(path.join(storage, "plugins"))
+populateDirectory(path.join(storage, "hashlist"))
 
 // Settings storage
 import db from 'node-persist'
@@ -50,7 +54,7 @@ db.init({
 })
 
 // Intialise MDC list
-const list = mdc.list.MDCList.attachTo(document.querySelector('.mdc-list'))
+const list = mdc.list.MDCList.attachTo($('.main--drawer-content').get(0))
 
 // Fix focusing
 list.wrapFocus = true
@@ -66,6 +70,16 @@ $.each($(".mdc-icon-button[data-mdc-auto-init='MDCRipple']"), (_, {
     MDCRipple.unbounded = true
 })
 
+// Attach snackbar
+const snackbar = new mdc.snackbar.MDCSnackbar($('.main--snackbar').get(0))
+
+// Display snackbar message
+const snackBarMessage = (message) => {
+    snackbar.close()
+    snackbar.labelText = message
+    snackbar.open()
+}
+
 // Bloom filter functionality
 import {
     BloomFilter
@@ -79,6 +93,19 @@ let hashes = new BloomFilter(
 
 // Line by line reader
 import LineByLineReader from "line-by-line"
+
+// Request parameters
+const requestParams = (url, json = false) => {
+    return {
+        url: url,
+        json: json,
+        gzip: true,
+        method: "GET",
+        headers: {
+            "User-Agent": "rosav (nodejs)"
+        }
+    }
+}
 
 // Line reader
 const hlr = new LineByLineReader(path.join(storage, "hashlist.txt"), {
@@ -126,6 +153,54 @@ import dayjs from "dayjs"
 // MD5 from file
 import MD5File from "md5-file"
 
+// If scan start triggered
 $(".scan--start").click(() => {
+    // Switch to scanning tab
     app.setActiveTab('scanning')
+})
+
+const manageSettings = (el, name) => {
+    if (el.hasClass("mdc-select")) {
+        const select = el.get(0).MDCSelect
+        if (db.getItem(name)) select.value = db.getItem(name)
+        select.listen('MDCSelect:change', () => {
+            db.setItem(name, select.value)
+        })
+    } else if (el.hasClass("mdc-text-field")) {
+        const textField = el
+        if (db.getItem(name)) textField.get(0).MDCTextField.value = db.getItem(name)
+        textField.find("input").on("input", () => {
+            db.setItem(name, textField.get(0).MDCTextField.value)
+        })
+    }
+}
+
+manageSettings($(".settings--update-behaviour"), "update-behaviour")
+manageSettings($(".settings--regex-matching"), "regex-matching")
+
+// Execute plugins
+fs.readdir(path.join(storage, "plugins"), (err, items) => {
+    if (err) {
+        snackBarMessage(`Failed to load plugins because ${err}`)
+    }
+    items.forEach((dir) => {
+        (() => {
+            if (dir.endsWith(".js")) {
+                fs.readFile(path.join(storage, "plugins", dir), 'utf8', (err, contents) => {
+                    if (err) {
+                        snackBarMessage(`Failed to load ${dir} because ${err}`)
+                    } else {
+                        (() => {
+                            try {
+                                eval(contents)
+                            } catch (err) {
+                                snackBarMessage(`Failed to load ${dir} because ${err}`)
+                            }
+                        })()
+                        snackBarMessage(`Successfully loaded ${dir}`)
+                    }
+                })
+            }
+        })()
+    })
 })
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Modules to control application life and create native browser window
 const {
     app,
-    session,
+    Menu,
+    Tray,
     BrowserWindow
 } = require('electron')
 
@@ -9,18 +10,57 @@ const {
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow() {
-    
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 960,
         height: 680,
+        icon: 'icon.ico',
         frame: false,
         resizable: false,
         contextIsolation: true
     })
 
-    // mainWindow.setMenu(null)
+    mainWindow.on('minimize', event => {
+        event.preventDefault()
+        mainWindow.hide()
+    })
+
+    mainWindow.on('close', event => {
+        if (!app.isQuiting) {
+            event.preventDefault()
+            mainWindow.hide()
+        }
+
+        return false
+    })
+
+    tray = new Tray('icon.ico')
+
+    const contextMenu = Menu.buildFromTemplate([{
+            label: 'Open ROS AV',
+            click() {
+                mainWindow.show()
+            }
+        },
+        {
+            label: 'Quit ROS AV',
+            click() {
+                app.isQuiting = true
+                app.quit()
+            }
+        }
+    ])
+
+    tray.on('click', () => {
+        tray.popUpContextMenu()
+    })
+
+    tray.setToolTip('ROS AV')
+    tray.setContextMenu(contextMenu)
 
     // and load the index.html of the app.
     mainWindow.loadFile('index.html')
@@ -29,21 +69,16 @@ function createWindow() {
     // mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
     })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+})
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -51,7 +86,7 @@ app.on('window-all-closed', function() {
     }
 })
 
-app.on('activate', function() {
+app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
