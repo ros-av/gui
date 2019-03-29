0ed49ba9e918201e2f637529f743c1282084715c
codefactor-io <support@codefactor.io>
2019-03-29 10:19:05 +0000

    [CodeFactor] Apply fixes to commit 002c2f6

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,9 @@
 const {
-    remote
-} = require("electron")
+    remote,
+} = require("electron");
 
 remote.getCurrentWebContents().once('dom-ready', () => {
-    require("electron-compile/lib/initialize-renderer").initializeRendererProcess(remote.getGlobal("globalCompilerHost").readOnlyMode)
+    require("electron-compile/lib/initialize-renderer").initializeRendererProcess(remote.getGlobal("globalCompilerHost").readOnlyMode);
 
-    require("./app.js")
-})
+    require("./app.js");
+});
